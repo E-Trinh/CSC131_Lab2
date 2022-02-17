52ce2ab295f3445036065da1bef703fb74c12ac0
Nahefa <99785550+Nahefa@users.noreply.github.com>
2022-02-16 16:36:37 -0800

    Update Hello.java

--- CSC131_Lab2/src/Hello.java
+++ CSC131_Lab2/src/Hello.java
@@ -8,7 +8,7 @@ public class Hello {
 		System.out.println("301360943 - This line was added by Ethan Trinh.");
 		System.out.println("301360904 - This line was added by Wilson Toy.");
 		System.out.println("302803241 - This line was added by Matthew Dominguez.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("302296865 - This line was added by Nahefa Sayed.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
